3e0191be0b95711dd6773a44ff47f35c247bae73
jerryntom <jaroslawtomczuk.ofi@gmail.com>
2022-11-08 13:58:08 +0100

    01 Added Policz function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b) {
+    return a+b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Wynik dodawania:" << Policz(10, 12) << std::endl;
     return 0;
 }
